b8249f49112fc373dbb22029475b6d987e62f8ca
Mugesh-13 <cmugesh000@gmail.com>
2025-04-07 16:18:12 +0530

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -3,21 +3,19 @@ package recursion;
 import java.util.Scanner;
 
 public class DecimalToBinary {
-	StringBuilder sb=new StringBuilder();
-	public String toBinary(int n) {
+	public void toBinary(int n) {
 		if(n>0) {
-			sb.insert(0,(n%2)+"");
-			n/=2;
-			toBinary(n);
+			toBinary(n/2);
+			System.out.print(n%2);
 		}
-		return sb.toString();
+		
 	}
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter the Number");
 		int num=scan.nextInt();
 		DecimalToBinary obj=new DecimalToBinary();
-		System.out.println(obj.toBinary(num));
+		obj.toBinary(num);
 	}
 
 }
